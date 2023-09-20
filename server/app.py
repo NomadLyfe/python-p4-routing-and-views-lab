cb01d059767d3c8713477721441ea9384e9e1fce
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-09-19 22:11:56 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,30 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<param>')
+def print_string(param):
+    print(param)
+    return f'{param}'
+
+@app.route('/count/<param>')
+def count(param):
+    return ''.join([str(i) + '\n' for i in range(int(param))])
+
+@app.route('/math/<num1>/<op>/<num2>')
+def math(num1, op, num2):
+    n1, n2 = int(num1), int(num2)
+    dict = {
+        '+': n1 + n2,
+        '-': n1 - n2,
+        '*': n1 * n2,
+        'div': n1 / n2,
+        '%': n1 % n2
+    }
+    return str(dict[op])
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
